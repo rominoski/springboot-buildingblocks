6dbf556449d974b34916a9d03398055d8d87d5de
Daniela Lira <daniela.lira@gmail.com>
2023-05-16 17:59:27 -0400

    Hello World Base

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -1,7 +1,8 @@
-package com.stacksimplify.restservices.springbootbuildingblocks;
+package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
