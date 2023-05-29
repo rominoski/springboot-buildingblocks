1dc861a99561eda26392577c65947e8edf07d382
Daniela Lira <daniela.lira@gmail.com>
2023-05-29 15:33:20 -0400

    ResponseStatusException First Commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserExistException.java
@@ -0,0 +1,16 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserExistException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4979446186685543171L;
+
+	public UserExistException(String message) {
+		super(message);
+		
+	}
+	
+
+}
--- src/main/java/com/stacksimplify/restservices/services/UserService.java
+++ src/main/java/com/stacksimplify/restservices/services/UserService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.exceptions.UserExistException;
+import com.stacksimplify.restservices.exceptions.UserNotFoundException;
 import com.stacksimplify.restservices.repositories.UserRepository;
 
 //Service
@@ -23,27 +27,52 @@ public class UserService {
 	}
 	
 	//Create User Method	
-	public User createUser(User user) {
+	public User createUser(User user)  throws UserExistException{
+		//if user exist using username
+		User existingUser= userRepository.findByUsername(user.getUsername());
+		//if not exists throw UserExistException
+		if(existingUser!=null) {
+			throw new UserExistException("User already exist in repository");
+		}
+		 
 		return userRepository.save(user);
 	}
-	//getUserById
 	
-	public Optional<User> getUserById(Long id) {
+	//getUserById	
+	public Optional<User> getUserById(Long id) throws UserNotFoundException {
 		Optional<User> user=userRepository.findById(id);
+		
+		if(!user.isPresent())
+		{
+			throw new UserNotFoundException("User not found in user Repository");
+		}
+		
 		return user;
 	}
 	
 	//Update User By Id	
-	public User updateUserById(Long id,User user) {
+	public User updateUserById(Long id,User user) throws UserNotFoundException {
+		Optional<User> optionalUser=userRepository.findById(id);
+		
+		if(!optionalUser.isPresent())
+		{
+			throw new UserNotFoundException("User not found in user Repository, provide the correct user id");
+		}
+		
 		user.setId(id);
 		return userRepository.save(user);		
 	}
 	
 	//DeleteUserById
 	public void deleteUserById(Long id) {
-		if(userRepository.findById(id).isPresent()) {
-			userRepository.deleteById(id);
+		
+		Optional<User> optionalUser=userRepository.findById(id);
+		
+		if(!optionalUser.isPresent())
+		{
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User not found in user Repository, provide the correct user id");
 		}
+		userRepository.deleteById(id);
 	}
 	
 	//getUserByUsername
